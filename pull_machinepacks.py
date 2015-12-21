cd7c70b1962a045048f4b3ef7a3bdb9ef0dcf512
Zhe Brah <zhebrah@gmail.com>
2015-12-21 12:12:15 +0100

    simplified

--- pull_machinepacks.py
+++ pull_machinepacks.py
@@ -1,24 +1,25 @@
-from bs4 import BeautifulSoup
+import requests
 from jinja2 import Template
 
-html_doc = open('machinepacks.html').read()
-soup = BeautifulSoup(html_doc)
+MACHINEPACKS_URL = 'http://node-machine.org/machinepacks'
 
-urls = filter(lambda x: x is not None and 'http://node-machine.org/machinepack' in x, map(lambda x: x.get('href'), soup.find_all('a')))
-packages = {url.replace('http://node-machine.org/', '') for url in urls}
-blacklisted = {
+BLACK_LIST = {
     'machinepack-weather',
     'machinepack-azure',
     'machinepack-mongodb',
     'machinepack-thumb',  # imagemagick
 }
-packages -= blacklisted
 
-for package in packages:
-    print package
 
-with open('package.json.j2', 'r') as f:
-    template = Template(f.read())
+def get_machinepacks_set():
+    r = requests.get(MACHINEPACKS_URL)
+    packages = {i['npmPackageName'] for i in r.json()}
+    return sorted(packages - BLACK_LIST)
 
-with open('package.json', 'w+') as f:
-    f.write(template.render(machinepacks=packages))
+
+if __name__ == '__main__':
+    with open('package.json.j2', 'r') as f:
+        template = Template(f.read())
+
+    with open('package.json', 'w+') as f:
+        f.write(template.render(machinepacks=get_machinepacks_set()))
